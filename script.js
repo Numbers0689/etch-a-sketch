f81cb0f4f9e8ae1e845af1f3aa635dd351862da9
Numbers0689 <firewatchsai@gmail.com>
2023-12-04 21:48:43 +0530

    Add hover and range features

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-let sq = 16;
+const input = document.querySelector("#rangeSize")
 
-const container = document.querySelector('div');
-for (let i = 0; i < sq; i++) {
-    for (let j = 0; j < sq; j++) {
-        const element = document.createElement('div');
-        element.classList.add("grid");
-        container.appendChild(element);
+input.addEventListener('change', () => {
+    let sq = input.value;
+    const container = document.querySelector('div');
+    for (let i = 0; i < sq; i++) {
+        for (let j = 0; j < sq; j++) {
+            const element = document.createElement('div');
+            element.classList.add("grid");
+            container.appendChild(element);
+        }
     }
-}
+});
+
+
+
+console.log(input.value)
