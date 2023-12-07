c82dba308eea58f2cb10fe6f7f5c4f508edc88ea
Numbers0689 <firewatchsai@gmail.com>
2023-12-07 23:16:22 +0530

    Add mouseover effect and fix grid

--- script.js
+++ script.js
@@ -1,33 +1,49 @@
 const input = document.querySelector(".size")
 const container = document.querySelector('div');
 
-for (let i = 0; i < 32; i++) {
-    for (let j = 0; j < 32; j++) {
-        const element = document.createElement('div');
-        element.classList.add("grid");
-        container.appendChild(element);
+function gridMakes(sq) {
+    for (let i = 0; i < sq; i++) {
+        const bigElement = document.createElement('div');
+        for (let j = 0; j < sq; j++) {
+            const element = document.createElement('div');
+            element.classList.add("grid");
+            bigElement.appendChild(element);
+        }
+        bigElement.classList.add("containerTwo");
+        container.appendChild(bigElement);
     }
 }
 
+gridMakes(32);
 
 input.addEventListener('click', () => {
     let sq = prompt("Enter Grid Size (16 - 128): ");
     if(sq < 16){
         sq = 16;
     }
-    else if(sq > 128){
-        sq = 128;
+    else if(sq > 64){
+        sq = 64;
     }
 
     while (container.lastElementChild) {
         container.removeChild(container.lastElementChild)
     }
-    for (let i = 0; i < sq; i++) {
-        for (let j = 0; j < sq; j++) {
-            const element = document.createElement('div');
-            element.classList.add("grid");
-            container.appendChild(element);
-        }
-    }
+    gridMakes(sq);
 
+    const gridDiv = document.querySelectorAll('.grid');
+
+    gridDiv.forEach(grid => {
+        grid.addEventListener('mouseover', () => {
+            grid.classList.add('colored');
+        });
+    });
+
+});
+
+const gridDiv = document.querySelectorAll('.grid');
+
+gridDiv.forEach(grid => {
+    grid.addEventListener('mouseover', () => {
+        grid.classList.add('colored');
+    });
 });
