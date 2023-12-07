f56c64c7270a825f20d1b27e687c3209e0825cb2
Numbers0689 <firewatchsai@gmail.com>
2023-12-07 20:09:05 +0530

    Switch to button and prompt

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-const input = document.querySelector("#rangeSize")
+const input = document.querySelector(".size")
+const container = document.querySelector('div');
 
-input.addEventListener('change', () => {
-    let sq = input.value;
-    const container = document.querySelector('div');
+for (let i = 0; i < 32; i++) {
+    for (let j = 0; j < 32; j++) {
+        const element = document.createElement('div');
+        element.classList.add("grid");
+        container.appendChild(element);
+    }
+}
+
+
+input.addEventListener('click', () => {
+    let sq = prompt("Enter Grid Size (16 - 128): ");
+    if(sq < 16){
+        sq = 16;
+    }
+    else if(sq > 128){
+        sq = 128;
+    }
+
+    while (container.lastElementChild) {
+        container.removeChild(container.lastElementChild)
+    }
     for (let i = 0; i < sq; i++) {
         for (let j = 0; j < sq; j++) {
             const element = document.createElement('div');
@@ -10,8 +29,5 @@ input.addEventListener('change', () => {
             container.appendChild(element);
         }
     }
+
 });
-
-
-
-console.log(input.value)
